5f8e485f0811a9b88581f1e695f3a70196f31f56
Gleb Salmanov <nullcaller@gmail.com>
2026-03-08 09:45:20 +0300

    Convert YNDX-00591 & YNDX-00592 quirk to v2

--- zhaquirks/yandex/yandex_curtain_motors.py
+++ zhaquirks/yandex/yandex_curtain_motors.py
@@ -5,33 +5,17 @@ from __future__ import annotations
 from collections.abc import Coroutine
 from typing import Any, Final
 
-from zigpy.profiles import zgp, zha
-from zigpy.quirks import CustomCluster, CustomDevice
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.quirks.v2.homeassistant.number import NumberDeviceClass
 import zigpy.types as t
 from zigpy.zcl.clusters.closures import WindowCovering
-from zigpy.zcl.clusters.general import (
-    Basic,
-    GreenPowerProxy,
-    Groups,
-    Identify,
-    Ota,
-    Scenes,
-)
-from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.foundation import (
     ZCL_CLUSTER_REVISION_ATTR,
     GeneralCommand,
     ZCLAttributeDef,
 )
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.yandex import YANDEX, YANDEX_MANUFACTURER_CODE_2, YandexType_VelocityLift
 
 
@@ -54,7 +38,7 @@ class YandexWindowCovering(CustomCluster, WindowCovering):
         unknown_attribute_61440: Final = ZCLAttributeDef(
             id=0xF000,
             type=t.Bool,
-            access="rw",
+            access="r",
             is_manufacturer_specific=True,
         )
         max_position: Final = ZCLAttributeDef(
@@ -97,62 +81,36 @@ class YandexWindowCovering(CustomCluster, WindowCovering):
         )
 
 
-class YandexCurtainMotor(CustomDevice):
-    """YNDX-00591 and YNDX-00592 curtain motors."""
-
-    signature = {
-        MODELS_INFO: [(YANDEX, "YNDX-00591"), (YANDEX, "YNDX-00592")],
-        ENDPOINTS: {
-            # <SimpleDescriptor endpoint=1 profile=260 device_type=514
-            # device_version=0
-            # input_clusters=[0, 3, 4, 5, 258, 2821]
-            # output_clusters=[3, 25]>
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    WindowCovering.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
-            },
-            # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
-            # device_version=0
-            # input_clusters=[]
-            # output_clusters=[33]>
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    YandexWindowCovering,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
-            },
-        },
-    }
+(
+    QuirkBuilder(YANDEX, "YNDX-00591")
+    .applies_to(YANDEX, "YNDX-00591")
+    .applies_to(YANDEX, "YNDX-00592")
+    .enum(
+        attribute_name=YandexWindowCovering.AttributeDefs.velocity_lift.name,
+        enum_class=YandexType_VelocityLift,
+        cluster_id=YandexWindowCovering.cluster_id,
+        translation_key="velocity_lift",
+        fallback_name="Velocity (Lift)",
+    )
+    .number(
+        attribute_name=YandexWindowCovering.AttributeDefs.max_position.name,
+        cluster_id=YandexWindowCovering.cluster_id,
+        min_value=0,
+        max_value=255,
+        step=1,
+        device_class=NumberDeviceClass.DISTANCE,
+        translation_key="max_position",
+        fallback_name="Maximum Position",
+    )
+    .number(
+        attribute_name=YandexWindowCovering.AttributeDefs.min_position.name,
+        cluster_id=YandexWindowCovering.cluster_id,
+        min_value=0,
+        max_value=255,
+        step=1,
+        device_class=NumberDeviceClass.DISTANCE,
+        translation_key="min_position",
+        fallback_name="Minimum Position",
+    )
+    .add_to_registry()
+)
